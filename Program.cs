a51a554431ac3ac299a6cc7e56b562585da7f33d
_Coder_Naren <narendrajalamkar8399@gmail.com>
2024-11-25 15:28:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_code2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Wish user
+            string name;
+            Console.WriteLine("Enter Your Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Good Evening" + name);
+            Console.ReadKey();
+        }
+    }
+}
